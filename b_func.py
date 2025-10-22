07eb88f6fe4d27c9627ca084dd090a2a3fa11ae9
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-10-22 10:42:50 +0700

    Refactored b_func.py

--- b_func.py
+++ b_func.py
@@ -19,7 +19,7 @@ def get_addresses(program_str: str) -> tuple[dict[int, int], dict[int, int]]:
     for char in range(len(program_str)):
         if program_str[char] == '[':
             i: int = char + NEXT
-            bracket_sum = 0
+            bracket_sum: int = 0
             while bracket_sum != LOOP_END:
                 match program_str[i]:
                     case '[':
@@ -31,7 +31,7 @@ def get_addresses(program_str: str) -> tuple[dict[int, int], dict[int, int]]:
             closed_br[i + PREV] = char + NEXT
     return open_br, closed_br
 
-def display_program_pointer(length: int,
+def display_pointer_p(length: int,
                             pos: int) -> str:
     return ' ' * pos + '^' + ' ' * (length - pos - 1)
 
@@ -42,7 +42,7 @@ def print_data(program_str: str,
                cell: int,
                steps: int) -> None:
     print('\n', program_str, '\n',
-          display_program_pointer(length, pos_p), '\n',
+          display_pointer_p(length, pos_p), '\n',
           f'{chr(OFF + cell)}, {pos_t}, {pos_p}, {steps}, ', sep='')
 
 def clean_str(program_str: str) -> str:
@@ -53,48 +53,48 @@ def clean_str(program_str: str) -> str:
                 new_str += char
     return new_str
 
+def goto(address_list: dict[int: int],
+         addresses_start: dict[int: int],
+         pos_p: int) -> tuple[int, bool]:
+    for address in addresses_start:
+        if address == pos_p:
+            pos_p = address_list[address]
+            return pos_p, True
+
 def get_output(b_input: tuple[int, str],
                tape: list[int]) -> None:
     num_states: int = b_input[NUM_STATES]
     program_str: str = clean_str(b_input[PROGRAM_STR]) + HALT
     program_str_len: int = len(program_str)
-    tape_pointer: int = len(tape) >> 1
-    program_pointer: int = 0
+    pointer_t: int = len(tape) >> 1
+    pointer_p: int = 0
     flag: bool = False
     steps: int = 0
     open_br, closed_br = get_addresses(program_str)
     open_br_keys: list[int] = list(open_br.keys())
     closed_br_keys: list[int] = list(closed_br.keys())
     print(open_br, closed_br)
-    while program_str[program_pointer] != HALT:
-        print_data(program_str, program_str_len, program_pointer,
-                   tape_pointer, tape[tape_pointer], steps)
-        match program_str[program_pointer]:
+    while program_str[pointer_p] != HALT:
+        print_data(program_str, program_str_len, pointer_p,
+                   pointer_t, tape[pointer_t], steps)
+        match program_str[pointer_p]:
             case '!':
-                tape[tape_pointer] += 1
-                tape[tape_pointer] %= num_states
+                tape[pointer_t] += 1
+                tape[pointer_t] %= num_states
             case '<':
-                tape_pointer -= 1
+                pointer_t -= 1
             case '>':
-                tape_pointer += 1
+                pointer_t += 1
             case '[':
-                if tape[tape_pointer] == 0:
-                    for address in open_br_keys:
-                        if address == program_pointer:
-                            program_pointer = open_br[address]
-                            flag = True
-                            break
+                if tape[pointer_t] == 0:
+                    pointer_p, flag = goto(open_br, open_br_keys, pointer_p)
             case ']':
-                if tape[tape_pointer] != 0:
-                    for address in closed_br_keys:
-                        if address == program_pointer:
-                            program_pointer = closed_br[address]
-                            flag = True
-                            break
+                if tape[pointer_t] != 0:
+                    pointer_p, flag = goto(closed_br, closed_br_keys, pointer_p)
         if not flag:
-            program_pointer += 1
+            pointer_p += 1
         flag = False
         steps += 1
-    print_data(program_str, program_str_len, program_pointer,
-               tape_pointer, tape[tape_pointer], steps)
+    print_data(program_str, program_str_len, pointer_p,
+               pointer_t, tape[pointer_t], steps)
     return None
