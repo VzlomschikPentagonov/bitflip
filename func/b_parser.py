23da3d5fa439b61ccdd75c48f152a5450892b51a
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-10-23 22:04:36 +0700

    Added defines

--- func/b_parser.py
+++ func/b_parser.py
@@ -14,8 +14,27 @@ def read_input_file() -> tuple[int, str]:
     return (int(input_file_data[NUM_STATES]),
             get_code(input_file_data))
 
+def read_defines_file() -> dict[str, str]:
+    input_file: TextIO = open("defines.txt")
+    input_file_data: list[str] = input_file.readlines()
+    defines: dict[str, str] = {}
+    for line in input_file_data:
+        if match(RM_DEFFILE, line):
+            define: str = ""
+            code: str = ""
+            char: int = 0
+            while line[char] != ':':
+                define += line[char]
+                char += 1
+            char += 1
+            for c_char in range(char + 1, len(line)):
+                code += line[c_char]
+            defines[define] = code
+    return defines
+
 def get_sub_string(program_str: str,
-                   pos: int) -> tuple[int, str]:
+                   pos: int,
+                   defines: dict[str, str]) -> tuple[int, str]:
     sub_string: str = ""
     code: str = ""
     repeat: str = ""
@@ -29,23 +48,22 @@ def get_sub_string(program_str: str,
                 code += char
             if char.isdigit():
                 repeat += char
+        return i, code * int(repeat)
     elif match(RM_DEFINE, sub_string, ASCII):
-        for char in sub_string:
-            if char in CHAR_SET:
-                code += char
-            if char.isdigit():
-                repeat += char
-    return i, code * int(repeat)
+        for define in defines.keys():
+            if sub_string == define:
+                return i, defines[define]
 
 def clean_str(program_str: str) -> str:
     new_str: str = ""
     i: int = 0
+    defines: dict[str, str] = read_defines_file()
     while i < len(program_str):
         match program_str[i]:
             case '!' | '<' | '>' | '[' | ']':
                 new_str += program_str[i]
             case '{':
-                i, sub_str = get_sub_string(program_str, i)
+                i, sub_str = get_sub_string(program_str, i, defines)
                 new_str += sub_str
         i += 1
     return new_str
