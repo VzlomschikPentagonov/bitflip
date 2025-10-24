9ce4ab4b9aeaa1297a84e22d8b4189d1922e1ea5
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-10-24 17:24:39 +0700

    Finshed header file saving and loading

--- func/mf_func.py
+++ func/mf_func.py
@@ -1,15 +1,32 @@
 from typing import TextIO
 from bitflip.func.b_parser import get_code
+from os import listdir
+
+def read_file(file: TextIO) -> list[str]:
+    file.seek(0)
+    file_data: list[str] = file.readlines()
+    file_data[0:0] = [""]
+    return file_data
 
 def make_file(name: str) -> None:
     input_file: TextIO = open("input.txt")
-    new_file_data: TextIO = open(f"./code/{name}.bflp", "w+t")
+    defines_file: TextIO = open("defines.txt")
+    new_file: TextIO = open(f"./code/{name}.bflp", "w+t")
     input_file_data: list[str] = input_file.readlines()
-    new_file_data.write(get_code(input_file_data))
+    if len(defines_file.readlines()) != 0:
+        header_file: TextIO = open(f"./code/{name}.hbflp", "w+t")
+        header_file_data: list[str] = read_file(defines_file)
+        header_file.write(get_code(header_file_data))
+    new_file.write(get_code(input_file_data))
 
 def load_file(name: str) -> None:
     input_file: TextIO = open("input.txt", "w+t")
-    read_file: TextIO = open(f"./code/{name}.bflp")
-    load_file_data: list[str] = read_file.readlines()
-    load_file_data[0:0] = [""]
-    input_file.write("2\n" + get_code(load_file_data))
+    defines_file: TextIO = open("defines.txt", "w+t")
+    code_file: TextIO = open(f"./code/{name}.bflp")
+    header_file_data: list[str] = ["", ""]
+    if f"{name}.hbflp" in listdir("./code/"):
+        header_file: TextIO = open(f"./code/{name}.hbflp")
+        header_file_data: list[str] = read_file(header_file)
+    code_file_data: list[str] = read_file(code_file)
+    input_file.write("2\n" + get_code(code_file_data))
+    defines_file.write(get_code(header_file_data))
