a396936f8b4a8923351056d9e73f6b95c4473fcd
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-10-31 01:28:36 +0700

    .

--- func/b_misc.py
+++ func/b_misc.py
@@ -1,7 +1,7 @@
 from bitflip.func.b_constants import *
 
 def display_pointer_p(length: int,
-                            pos: int) -> str:
+                      pos: int) -> str:
     return ' ' * pos + '^' + ' ' * (length - pos - 1)
 
 def print_data(program_str: str,
@@ -13,3 +13,7 @@ def print_data(program_str: str,
     print('\n', program_str, '\n',
           display_pointer_p(length, pos_p), '\n',
           f'{chr(OFF + cell)}, {pos_t}, {pos_p}, {steps}', sep='')
+
+def print_tape(tape: list[int]):
+    tape_str: list[str] = [chr(OFF + cell) for cell in tape]
+    print("".join(tape_str))
