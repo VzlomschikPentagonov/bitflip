7b09f1b6e67a96166b319c7eb333ddf348645f65
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-11-03 22:19:19 +0700

    .

--- bitfrip.py
+++ bitfrip.py
@@ -5,7 +5,7 @@ def main() -> None:
     b_input: tuple[int, str] = read_input_file()
     defines: dict[str: str] = read_defines_file()
     tape: list[int] = [0] * 0x100
-    get_output(b_input, tape)
+    get_output(b_input, tape, defines)
     return None
 
 if __name__ == "__main__":
