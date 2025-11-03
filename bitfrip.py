4c59e86030b15761a20d5a5fccd85ab8b132323e
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-11-03 14:56:39 +0700

    Update bitfrip.py

--- bitfrip.py
+++ bitfrip.py
@@ -3,7 +3,7 @@ from func.b_intepreter import get_output
 
 def main() -> None:
     b_input: tuple[int, str] = read_input_file()
-    defines: dict[str: str] = {}
+    defines: dict[str: str] = read_defines_file()
     tape: list[int] = [0] * 0x100
     get_output(b_input, tape)
     return None
