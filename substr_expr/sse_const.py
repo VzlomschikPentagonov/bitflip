783a17b0bd0aeb43c6a84570d56031476f677872
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-11-11 14:44:44 +0700

    Update sse_const.py

--- substr_expr/sse_const.py
+++ substr_expr/sse_const.py
@@ -1,6 +1,9 @@
+# various constants
 SUBSTR_ARGS: int = 0
 NUM_ARGS: int = 2
 START: int = 0
+
+# regex strings
 RE_SUBSTR_D1: str = r"^[1-9].*\d*$"
 RE_SUBSTR_D2: str = r"^\d+$"
 RE_SUBSTR: str = r"^([\w\-+\^/\\,\s]*\|\d*)$"
