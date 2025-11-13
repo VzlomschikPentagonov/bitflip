64d1579cdc3bac96e09c6427a2eca3b6086b5e27
Андреюшин Тимофей Игоревич ИП-415 <5959jhihguoygbio54@gmail.com>
2025-11-13 23:26:44 +0700

    Refaco

--- bitfrip.py
+++ bitfrip.py
@@ -3,9 +3,9 @@ from func.b_intepreter import get_output
 
 def main() -> None:
     b_input: tuple[int, str, int] = read_input_file()
-    defines: dict[str: str] = read_include_file()
+    sub_strs: dict[str: str] = read_include_file()
     tape: list[int] = [0] * 0x100
-    get_output(b_input, tape, defines)
+    get_output(b_input, tape, sub_strs)
     return None
 
 if __name__ == "__main__":
