cd090e27255d4b32d1393726edcd669e3a6ab1a3
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-11-20 23:39:26 +0700

    Update sse_classes.py

--- substr_expr/sse_classes.py
+++ substr_expr/sse_classes.py
@@ -1,7 +1,9 @@
 class Key:
-    def __init__(self, key: str = "",
+    def __init__(self, identifier: list[int] | str,
+                 keys: list[str],
                  entries: int = -1) -> None:
-        self.key: str = key
+        self.identifier: list[int] | str = identifier
+        self.keys: list[str] = key
         self.entries: int = entries
 
     def decrease_entries(self) -> None:
