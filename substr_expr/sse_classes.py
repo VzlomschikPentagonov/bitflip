eadc7f6376d287c14dbd6c60ef8f6fbd459d3442
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-11-25 17:02:04 +0700

    Delete substr_expr/sse_classes.py

--- substr_expr/sse_classes.py
+++
@@ -1,8 +0,0 @@
-class Arg:
-    def __init__(self, keys: list[str],
-                 entries: int = -1) -> None:
-        self.keys: list[str] = keys
-        self.entries: int = entries
-
-    def decrease_entries(self) -> None:
-        self.entries -= 1
