1ee55af8eb1f3a5c35195327c8b4234a2ef1c503
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-11-26 11:00:41 +0700

    Update sse_const.py

--- substr_expr/sse_const.py
+++ substr_expr/sse_const.py
@@ -11,7 +11,7 @@ PICK_LENGTH_N: int = 0
 # regex strings
 RE_SUBSTR_D1: str = r"^[1-9].*\d*$"
 RE_SUBSTR_D2: str = r"^\d+$"
-RE_SUBSTR: str = r"^([\w\-+\^/\\,\s]*\|\d*)$"
+RE_SUBSTR: str = r"^([\w\-+\^/\\,\s]*:\d*)$"
 RE_DIGIT_ARG: str = r"^(\d*(-\d+(\+\d+)?)?\^?\d*)$"
 RE_STARTW_ARG: str = r"^(/\w+)$"
 RE_ENDSW_ARG: str = r"^([\]\w+)$"
