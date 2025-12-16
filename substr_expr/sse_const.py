bc9900b680250cbc81dc150329af6755711bff01
VzlomschikPentagonov <5959jhihguoygbio54@gmail.com>
2025-12-16 17:11:59 +0700

    Update sse_const.py

--- substr_expr/sse_const.py
+++ substr_expr/sse_const.py
@@ -10,8 +10,8 @@ STR_DEFAULT: str = "%"
 # regex strings
 RE_SUBSTR_D1: str = r"^[1-9].*\d*$"
 RE_SUBSTR_D2: str = r"^\d+$"
-RE_SUBSTR_CHARSET: str = r"\^\-\w+,/\s\\"
+RE_SUBSTR_CHARSET: str = r"~\-\w+,/\s\\"
 RE_SUBSTR: str = r"^([%s]*;\d*)$" % RE_SUBSTR_CHARSET
-RE_DIGIT_ARG: str = r"^(\d*(-\d+(\+\d+)?)?\^?)$"
-RE_KEYWORD_ARG: str = r"^([/\\]\w+)$"
-RE_SPLIT_DIGIT_ARG: str = r"[\-+\^]"
+RE_DIGIT_ARG: str = r"^(~?\d*(-\d+(\+\d+)?)?)$"
+RE_KEYWORD_ARG: str = r"^(~?[/\\]?\w+)$"
+RE_SPLIT_DIGIT_ARG: str = r"[\-+~]"
